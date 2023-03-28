9dddbfafb65e9d1153dafcd58b9b76e779b10ba5
Abraão Araujo <96948947+Abraao4raujo@users.noreply.github.com>
2023-03-28 12:52:36 -0300

    interacao do botao menu

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ $('html, body').animate({
   scrollTop: targetOffset - 100
 	}, 500);
 })
+
+/* Abrir Menu Mobile */
+const btnMobile = document.getElementById('btnMobile')
+function toggleMenu(event) {
+  if(event.type === 'touchstart') event.preventDefault()
+  const nav = document.getElementById('nav')
+  nav.classList.toggle('active')
+  const active = nav.classList.contains('active')
+  event.currentTarget.setAttribute('aria-expended', active)
+  if(active){
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+  } else{
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
